8764b67b38a1a31ec71bbeedca3fa33bf43b532b
Mukesh-coder7 <mukeshbabuacharyaS@gmail.com>
2025-02-15 20:11:06 +0545

    profile edit

--- Design/Leaderboard/leaderboarduser.py
+++ Design/Leaderboard/leaderboarduser.py
@@ -4,7 +4,7 @@ def create_table(frame, data):
 
     headers = ["SN", "Username", "Score"]
     for col, header in enumerate(headers):
-        Label(frame, text=header, font=("Arial", 12, "bold"), bg="lightgrey", width=10).grid(row=0, column=col, padx=1, pady=1)
+        Label(frame, text=header, font=("Arial", 12, "bold"), bg="grey", width=10).grid(row=0, column=col, padx=1, pady=1)
 
     for row_idx, row_data in enumerate(data):
         for col_idx, value in enumerate(row_data):
@@ -16,14 +16,6 @@ root.title('Quiz Leaderboard')
 
 mainframe = Frame(root, bd=2, relief="ridge")
 mainframe.place(x=0, y=0, width=1000, height=800)
-
-topfr = Frame(root, bd=2, relief="ridge")
-topfr.place(x=0, y=0, width=1000, height=70)
-Label(topfr, text="LEADERBOARD", font=("Arial", 30, "bold"), fg='black').place(x=500, y=0)
-
-bottomfr = Frame(root, bd=2, relief="ridge")
-bottomfr.place(x=370, y=60, width=600, height=60)
-Label(bottomfr, text="FINAL STANDING", font=("Arial", 23, "bold"), fg='black').place(x=145, y=10)
 
 
 data = {
@@ -61,24 +53,24 @@ data = {
     ],
 }
 
-table_frame1 = Frame(root, bd=2, relief="ridge", bg="lightgrey")
-table_frame1.place(x=40, y=190, width=400, height=250)
-Label(root, text="Loksewa", font=('Arial', 14, 'bold'), fg='black').place(x=180, y=165)
+table_frame1 = Frame(root, bd=2, relief="ridge", bg="#2C3E50")
+table_frame1.place(x=40, y=135, width=390, height=250)
+Label(root, text="Loksewa", font=('Arial', 14, 'bold'), fg='black',bg='#E74C3C').place(x=40, y=100)
 create_table(table_frame1, data["Loksewa"])
 
-table_frame2 = Frame(root, bd=2, relief="ridge", bg="lightgrey")
-table_frame2.place(x=550, y=190, width=400, height=250)
-Label(root, text="CEE", font=('Arial', 14, 'bold'), fg='black').place(x=730, y=165)
+table_frame2 = Frame(root, bd=2, relief="ridge", bg="#2C3E50")
+table_frame2.place(x=550, y=135, width=390, height=250)
+Label(root, text="CEE", font=('Arial', 14, 'bold'), fg='black',bg='#E74C3C').place(x=550, y=100)
 create_table(table_frame2, data["CEE"])
 
-table_frame3 = Frame(root, bd=2, relief="ridge", bg="lightgrey")
-table_frame3.place(x=40, y=525, width=400, height=250)
-Label(root, text="IOE", font=('Arial', 14, 'bold'), fg='black').place(x=180, y=500)
+table_frame3 = Frame(root, bd=2, relief="ridge", bg="#2C3E50")
+table_frame3.place(x=40, y=525, width=390, height=250)
+Label(root, text="IOE", font=('Arial', 14, 'bold'), fg='black',bg='#E74C3C').place(x=40, y=490)
 create_table(table_frame3, data["IOE"])
 
-table_frame4 = Frame(root, bd=2, relief="ridge", bg="lightgrey")
-table_frame4.place(x=550, y=525, width=400, height=250)
-Label(root, text="Driving", font=('Arial', 14, 'bold'), fg='black').place(x=700, y=500)
+table_frame4 = Frame(root, bd=2, relief="ridge", bg="#2C3E50")
+table_frame4.place(x=550, y=525, width=390, height=250)
+Label(root, text="Driving", font=('Arial', 14, 'bold'), fg='black',bg='#E74C3C').place(x=550, y=490)
 create_table(table_frame4, data["Driving"])
 
 root.mainloop()
